792f8fa10db3f550dc348da38ea77b177aaee4b3
ihor.hordiichuk <ihor.hordiichuk@trinetix.con>
2023-11-18 23:47:41 +0100

    mock service fulfill method refactored

--- tests/firstTest.test.ts
+++ tests/firstTest.test.ts
@@ -24,8 +24,8 @@ test.describe('test suite', async () => {
 
     test('Mock dashboard', async ({dashboardPage, mockService}) => {
         await dashboardPage.goto()
-        const payload = JSON.stringify({norun: 4, passed: 6, failed: 5, total: 15})
-        await mockService.fulfillRequest('**/getstat/*').withPayload(payload)
+        const payload = JSON.stringify({norun: 4, passed: 6, failed: 5, total: 15, bobal: 35})
+        mockService.replaceResponse('**/getstat/*', {body: payload})
         await dashboardPage.refreshTestCasesDashboard()
         const dashboardData = await dashboardPage.getAllTestCaseStats()
         expect(dashboardData).toEqual({norun: 4, passed: 6, failed: 5, total: 15})
